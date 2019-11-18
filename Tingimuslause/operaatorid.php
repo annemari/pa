2c5be848f2ad7ee7b9bd9055bfd5118bdfe2eb4f
annem <annemari.eensaar@gmail.com>
2019-11-18 20:19:10 +0200

    Operaatorid

---
+++ Tingimuslause/operaatorid.php
@@ -0,0 +1,18 @@
+<?php
+// Operaatorid Anne-Mari
+$x = 3;
+$y = 2;
+// loogilised operaatorid
+var_dump($x == $y);
+echo '<br>';
+var_dump($x != $y);
+echo '<br>';
+var_dump($x < $y);
+echo '<br>';
+var_dump($x > $y);
+echo '<br>';
+var_dump($x >= $y);
+echo '<br>';
+var_dump($x <= $y);
+echo '<br>';
+
