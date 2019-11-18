857ec0eba5fa14ace4c01b03cf8a1f3fa75c92d1
annem <annemari.eensaar@gmail.com>
2019-11-18 20:19:28 +0200

    Suurem väiksem

---
+++ Tingimuslause/suurem_vaiksem.php
@@ -0,0 +1,15 @@
+<?php
+
+// muutujad
+$a = 2;
+$b = 3;
+
+// leiame kumb arv on suurem, või on arvud võrdsed
+if($a > $b) {
+    echo $a.' on suurem kui '.$b.'<br>';
+} elseif ($a < $b) {
+    echo $a.' on väiksem kui '.$b.'<br>';
+} else {
+    echo 'Arvud on võrdsed<br>';
+}
+?>
