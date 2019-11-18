825b27a5acb54402481dcf6c11abea73e35f226b
annem <annemari.eensaar@gmail.com>
2019-11-18 20:08:24 +0200

    Ülesanne 1

---
+++ Muutujad/ylesanne1.php
@@ -0,0 +1,22 @@
+<?php
+
+//Ülesanne 1 Anne-Mari
+$nimi = 'Anne-Mari';
+$vanus = 27;
+$sugu = 'naine';
+//Väljastamine
+echo 'Tere ' . $nimi . '!<br>';
+echo 'Sina oled ' . $vanus . ' aastane<br>';
+echo 'Sina oled ' . $sugu . '<br>';
+echo "\"It's Alright, Ma (I'm Only Bleeding)\" - Bob Dylan<br>";
+echo '&nbsp;<br>';
+
+echo '&nbsp;\ | /<br>';
+echo '@@ <br>';
+echo '&nbsp;&nbsp;* <br>';
+echo '\\"""/<br><br>';
+echo '&nbsp;<br>';
+echo '&nbsp;&nbsp;&nbsp;\___/<br>';
+echo '&nbsp;&nbsp;(@,@)<br>';
+echo '&nbsp;(&nbsp;(&nbsp;|&nbsp;|&nbsp;)&nbsp;)<br>';
+echo '__,,_,,__<br>';
