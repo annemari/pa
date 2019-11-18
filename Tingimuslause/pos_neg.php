249d0868398327536154080683fc96edb8db4d3e
annem <annemari.eensaar@gmail.com>
2019-11-18 20:23:32 +0200

    Positiivne negatiivne

---
+++ Tingimuslause/pos_neg.php
@@ -0,0 +1,14 @@
+<?php
+
+$arv = 7;
+
+if ($arv > 0) {
+
+    echo $arv . ' on positiivne<br>';
+} else if ($arv < 0) {
+
+    echo $arv . ' on negatiivne<br>';
+} else {
+
+    echo $arv . ' on null<br>';
+}
